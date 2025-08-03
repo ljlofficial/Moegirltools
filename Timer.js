04cb56b9e571af363f2f6924488a36c32197a0ed
LJL <ljlorljl@163.com>
2025-08-03 16:52:46 +0800

    update tools

--- Timer.js
+++
@@ -1,7 +0,0 @@
-//时间显示
-var offset = Number(mw.user.options.get("timecorrection").split("|")[1]);
-var $timer = $("<li id='pt-timer'><a title='当地时间' href='/Special:参数设置#mw-prefsection-rendering-timeoffset'></a></li>");
-$("#p-personal ul>li:last").after($timer.children().text(moment(mw.now()).utcOffset(offset).format("LTS")).end());
-timerInterval = setInterval(function() {
-	$("#p-personal ul>li:last").after($timer.children().text(moment(mw.now()).utcOffset(offset).format("LTS")).end());
-}, 1000);
---
+++ mwpanel.js
@@ -0,0 +1,27 @@
+// <pre>
+"use strict";
+$(() => {
+    const wgPageName = mw.config.get("wgRelevantPageName");
+    const items = {
+        "#t-upload": {
+            "t-expandtemplates": `<li id="t-expandtemplates" class="mw-list-item"><a href="/Special:展开模板?wpRemoveComments=1&wpInput={{${wgPageName}}}">${wgULS("展开模板", "展開模板")}</a></li>`,
+            "t-prefixindex": `<li id="t-prefixindex class="mw-list-item"><a href="/Special:前缀索引?prefix=${wgPageName}">${wgULS("前缀页面", "按詞頭查詢頁面")}</a></li>`,
+            "t-pagelog": `<li id="t-pagelog" class="mw-list-item"><a href="/Special:日志?page=${wgPageName}">${wgULS("页面日志", "頁面日誌")}</a></li>`,
+            "t-replacetext": `<li id="t-replacetext" class="sysop-show"><a href="/Special:替换文本">${wgULS("替换文本", "取代文字")}</a></li>`,
+        },
+        "#n-recentchanges": {
+            "n-log": `<li id="n-log" class="mw-list-item"><a href="/Special:日志" title="所有日志">${wgULS("所有日志", "所有日誌")}</a></li>`,
+        },
+    };
+    for (const t in items) {
+        const target = $(t);
+        for (const i in items[t]) {
+            if (!document.getElementById(i)) {
+                target.after(items[t][i]);
+            }
+        }
+    }
+    mw.loader.addStyleTag("#t-expandtemplates, #t-userlog, .ns-2 #t-pagelog, .ns-3 #t-pagelog, .ns--1 #t-pagelog {display:none;}.ns-10 #t-expandtemplates, .ns-2 #t-userlog, .ns-3 #t-userlog {display:list-item!important;}");
+    $("#t-log a").text(wgULS("用户日志", "使用者日誌", null, null, "用戶日誌"));
+});
+// </pre>
